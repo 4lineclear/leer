2c328616eca83bd693238b38523f24d5aa0b28dc
tamimhakimi <tamim.hakimi@ontariotechu.net>
2023-04-17 18:50:01 -0400

    Removed all work done, will start from new

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,6 @@
-use scrabble::Game;
-
 // TODO:https://stackoverflow.com/questions/59890270/how-do-i-overwrite-console-output
 // TODO:https://www.scrabblepages.com/scrabble/rules/
 //TODO:https://www.wordgamedictionary.com/sowpods/download/sowpods.txt
 fn main() {
-    let test = Game::new();
-    println!("{:#?}", test)
+    println!("Work in progress")
 }
